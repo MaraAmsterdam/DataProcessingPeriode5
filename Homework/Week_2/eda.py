5629cceea3842f0bbc50bed3149b46710a3b8173
Mara Oosterbaan <maraoosterbaan@wcw-stud-145-109-22-172.wireless.uva.nl>
2019-04-11 12:04:04 +0200

    Because deleting rows will influence the data analysis, I decided to keep the NaNs in data file and let pandas handle them

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,9 +1,31 @@
 import csv
 import pandas as pd 
 
-df = pd.read_csv('input.csv', sep=',')
+INPUT_FILE = 'input.csv' 
 
-print(df.loc[:,'Country'])
+
+if __name__ == "__main__":
+
+	# Load raw data
+	data = pd.read_csv(INPUT_FILE, sep=',')
+
+	# Select data of interest
+	df = data.loc[:,['Country',
+	                 'Region',
+	                 'Pop. Density (per sq. mi.)',
+	                 'Infant mortality (per 1000 births)',
+	                 'GDP ($ per capita) dollars']
+	              ]
+
+    # Remove white spaces before and after strings	              
+	for col in df:
+		df[col] = df[col].str.strip()
+
+	for index, row in df.iterrows():
+		if row.isna().any():
+			print(row)
+
+
 
 
 
