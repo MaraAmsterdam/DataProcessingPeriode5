e58bd8dc680ca8a62d3e76f45f317b26436e80ee
Mara Oosterbaan <maraoosterbaan@wcw-stud-145-109-10-100.wireless.uva.nl>
2019-05-09 15:51:49 +0200

    Build first data structure

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -12,6 +12,22 @@ var requests = [d3.json(teensInViolentArea),
  				d3.json(GDP)
  ];
 
+/*function selectData(data, dataselectie){
+
+	console.log(data);
+	console.log(dataselectie);
+    
+    Object.values(data.dataselectie).forEach(function(dataset) {
+    	//console.log(dataset);
+    	for (var i = 0; i < Object.values(dataset).length; i++){
+    		//console.log(Object.values(dataset)[i]);
+    		if ((Object.values(dataset)[i].Time == "2012") || (Object.values(dataset)[i].Year == "2012")){
+    			console.log(Object.values(dataset)[i].Country + ": " + Object.values(dataset)[i].Datapoint);
+    		}
+    	}	
+	});
+}*/ 
+
 Promise.all(requests).then(function(response) {
 
 
@@ -21,8 +37,97 @@ Promise.all(requests).then(function(response) {
       dataPregnancies : transformResponse(response[1]),
       dataGDP : transformResponseV2(response[2])
     };
+    var body = d3.select("body");
+ 	
+ 	var dataStruct = {
+ 					  "2012" : [],
+ 					  "2013" :[]
+ 	}
+ 		selectedData = {};
 
 
+
+    Object.values(dataScatterplot.dataGDP).forEach(function(dataset) {
+    	for (var i = 0; i < Object.values(dataset).length; i++){
+    		//console.log(Object.values(dataset)[i]);
+    		if ((Object.values(dataset)[i].Time == "2012") || (Object.values(dataset)[i].Year == "2012")){
+    			
+    			// Create variables to add to dict
+    			var country = Object.values(dataset)[i].Country;
+    			var datapointG = Object.values(dataset)[i].Datapoint
+    			selectedData[country] = {"datapointG" : datapointG};
+    			
+    		}
+
+    	}
+    });
+
+    Object.values(dataScatterplot.dataViolent).forEach(function(dataset) {
+    	//console.log(dataset);
+    	for (var i = 0; i < Object.values(dataset).length; i++){
+    		//console.log(Object.values(dataset)[i]);
+    		if ((Object.values(dataset)[i].Time == "2012") || (Object.values(dataset)[i].Year == "2012")){
+    			
+    			var country = Object.values(dataset)[i].Country;
+    			var datapointV = Object.values(dataset)[i].Datapoint
+
+    			// Add datapoint to excisting country
+    			if (selectedData[country]) {
+    		 		Object.assign(selectedData[country], {"datapointV" : datapointV});
+    			}
+    		}
+
+    	}	
+    });
+
+    Object.values(dataScatterplot.dataPregnancies).forEach(function(dataset) {
+
+    	for (var i = 0; i < Object.values(dataset).length; i++){
+
+    		if ((Object.values(dataset)[i].Time == "2012") || (Object.values(dataset)[i].Year == "2012")){
+    			
+    			//console.log(Object.values(dataset)[i].Country + ": " + Object.values(dataset)[i].Datapoint);
+    			var country = Object.values(dataset)[i].Country;
+    			var datapointP = Object.values(dataset)[i].Datapoint
+
+    			if (selectedData[country]) {
+    		 	Object.assign(selectedData[country], {"datapointP" : datapointP});
+    			}
+    		}
+
+    	}
+    });
+
+
+	Object.keys(selectedData).forEach(function(k){
+		var k = k;
+	});
+
+	
+
+	
+
+	
+
+
+    
+    /*
+    for (var j = 0; j < Object.keys(dataScatterplot).length; j++){
+    	dataset = Object.keys(dataScatterplot)[j];
+    	console.log(dataset);
+    	// .dataset does not work; due to asynchronity?
+		Object.values(dataScatterplot.dataset).forEach(function(subset) {
+		//console.log(dataset);
+			for (var i = 0; i < Object.values(subset).length; i++){
+				//console.log(Object.values(dataset)[i]);
+				if ((Object.values(subset)[i].Time == "2012") || (Object.values(subset)[i].Year == "2012")){
+					console.log(Object.values(subset)[i].Country + ": " + Object.values(subset)[i].Datapoint);
+				}
+			}
+		}); 
+	}
+	*/
+ 
 }).catch(function(e){
     throw(e);
 });
