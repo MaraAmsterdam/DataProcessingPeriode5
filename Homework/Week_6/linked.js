25c7b6a3150c129786b650dde3c3f92fdbd9e1df
Mara Oosterbaan <maraoosterbaan@Maras-MacBook-Air.local>
2019-05-22 16:29:16 +0200

    need to fix yscale but the first graph is ready

--- Homework/Week_6/linked.js
+++ Homework/Week_6/linked.js
@@ -1,5 +1,3 @@
-
-
 d3.json("data/workout_2016.json").then(function(data){
 
 	var margin = {top : 40, bottom : 60, left : 60, right: 100},
@@ -13,32 +11,29 @@ d3.json("data/workout_2016.json").then(function(data){
 				.append("g")
 				  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	// Setup x 
-	var x0 = d3.scaleBand()
+	// Setup x scales
+	var xScale = d3.scaleBand()
 				.rangeRound([0, width])
-				.paddingInner(0.1);
+				.paddingInner(0.1)
+				.domain(data.map(function(d){ return d.workoutfreq; }));
 
-	var x1 = d3.scaleBand()
-				.padding(0.05);
+	var xScale1 = d3.scaleBand()
+				.padding(0.05)
+				.domain(data.map(function(d){ return d.health; }))
+	   			.rangeRound([0, xScale.bandwidth()])
+	   			.padding(0.2);
 
 	var y = d3.scaleLinear()
 			   .rangeRound([height, 0])
 
-	var y1 = d3.scaleBand()
-
 	var z = d3.scaleOrdinal()
-			   .range(["#e34a33","#fdbb84", "#fee8c8"]);
+			   .range(["#e34a33","#fdbb84", "#fee8c8"])
+			   .domain(data.map(function(d){ return d.happiness; }));
+	var keys = z.domain();
 
 	var stack = d3.stack()
-	     .offset(d3.stackOffsetExpand);
+	     		   .offset(d3.stackOffsetExpand);
 
-	x0.domain(data.map(function(d){ return d.workoutfreq; }));
-	x1.domain(data.map(function(d){ return d.health; }))
-	   .rangeRound([0, x0.bandwidth()])
-	   .padding(0.2);
-
-	z.domain(data.map(function(d){ return d.happiness; }));
-	var keys = z.domain();
 
 	var groupedData = d3.nest()
 						 .key(function(d) { return d.health + d.workoutfreq; })
@@ -47,15 +42,16 @@ d3.json("data/workout_2016.json").then(function(data){
 							var d2 = {health: d[0].health, workoutfreq: d[0].workoutfreq}
 							d.forEach(function(d){
 								d2[d.happiness] = d.value
+								//console.log(d2[d.happiness]);
 							})
-				      		console.log("rollup d", d, d2);
 				    		return d2;			
 						 })
 						 .entries(data)
 						 .map(function(d){ return d.value; });
 
-
+	console.log(groupedData);
 	var stackData = stack.keys(keys)(groupedData);
+	console.log(stackData);
 
 	var serie = svg.selectAll(".serie")
 				  .data(stackData)
@@ -66,27 +62,25 @@ d3.json("data/workout_2016.json").then(function(data){
 
 
 	serie.selectAll("rect")
-	      .data(function(d) { 
-	      	console.log(d);
-	      	return d; })
+	      .data(function(d) { return d; })
 	      .enter()
 	      .append("rect")
 	        .attr("class", "serie-rect")
-			.attr("transform", function(d, i) { return "translate(" + x0(d.data.workoutfreq) + ",0)"; })
-			.attr("x", function(d) { return x1(d.data.health); })
+			.attr("transform", function(d, i) { return "translate(" + xScale(d.data.workoutfreq) + ",0)"; })
+			.attr("x", function(d) { return xScale1(d.data.health); })
 			.attr("y", function(d) { return y(d[1]); })
 			.attr("height", function(d) { return y(d[0]) - y(d[1]); })
-			.attr("width", x1.bandwidth())
+			.attr("width", xScale1.bandwidth())
 			.on("click", function(d, i){ console.log("serie-rect click d", i, d); });
 
 	svg.append("g")
 	  .attr("class", "axis x")
 	  .attr("transform", "translate(0," + height + ")")
-	  .call(d3.axisBottom(x0));
+	  .call(d3.axisBottom(xScale));
 
 	svg.append("g")
 	  .attr("class", "axis y")
-	  .call(d3.axisLeft(y).ticks(null, "s"))
+	  .call(d3.axisLeft(y).ticks(10))
 	 .append("text")
 	  .attr("x", 2)
 	  .attr("y", y(y.ticks().pop()) + 0.5)
@@ -115,31 +109,7 @@ d3.json("data/workout_2016.json").then(function(data){
 	      .attr("x", width - 24)
 	      .attr("y", 9.5)
 	      .attr("dy", "0.32em")
-	      .text(function(d) { return d; });
-		/*
-	var legend = svg.append("g")
-					 .data(stackData)
-					 .attr("class", "legend")
-					 .attr("transform", function(d) { 
-					 	console.log(d);
-					 	var d = d[d.length - 1]; 
-					 	return "translate(" +
-					 	(x0(d.data.workoutfreq) +
-					 	x1(d.data.health) + 
-					 	x1.bandwidth()) + "," +
-					 	((y(d[0]) + 
-					 	y(d[1])) / 2) + ")"});
+	      .text(function(d) { return d; });		
 
-	legend.append("line")
-			.attr("x1", -6)
-			.attr("x2", 6)
-			.attr("stroke", "#000");
-
-	legend.append("text")
-			.attr("x", 9)
-			.attr("dy", "0.35em")
-			.attr("fill", "#000")
-			.style("font", "10px sans-serif")
-			.text(function(d){ return d.key; });*/
 
 });
