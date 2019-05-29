951378faf958d29b77c5cc4d2a7f13063ab512a2
Mara Oosterbaan <maraoosterbaan@wcw-stud-145-109-7-225.wireless.uva.nl>
2019-05-29 12:56:20 +0200

    almost there...

--- Homework/Week_6/connectedScatter.js
+++ Homework/Week_6/connectedScatter.js
@@ -1,14 +1,11 @@
-// Function calls new svg and second graph
+// Function to create new SVG and Multiple line chart
 
 function connectedScatter(filepath){
 
 	d3.json(filepath).then(function(data){
 
-		console.log(data)
-
-
-		var margin = {top: 10, right: 100, bottom: 30, left: 30},
-		width = 500 - margin.left - margin.right,
+		var margin = {top : 40, bottom : 60, left : 60, right: 100},
+		width = 900 - margin.left - margin.right,
 		height = 400 - margin.top - margin.bottom;
 
 		var groups = [data[0].frequency, data[1].frequency, data[2].frequency];
@@ -24,19 +21,43 @@ function connectedScatter(filepath){
 						 	.domain(groups)
 						 	.range(["#fee6ce","#fdae6b","#e6550d"]);
 		var xScale = d3.scaleLinear()
-						.domain([0,4])
-						.range([0, width]);					
+						.domain([2014,2017])
+						.rangeRound([margin.left, width]);
+
+		/*var xScale = d3.scaleTime()
+				.domain([new Date(2014,0,1), new Date(2024,0,1)])
+				.range([0,10]);	*/
+
 		svg.append("g")
-			.attr("transform", "translate(" + margin.left + "," + height + ")")
-			.call(d3.axisBottom(xScale));
+			.attr("transform", "translate(0," + (height + margin.top) + ")")
+			.call(d3.axisBottom(xScale)
+					.ticks(4));
+
+
 
 		var yScale = d3.scaleLinear()
-					.domain([0, 100])
-					.range([height, 0]);
+						.domain([0, 100])
+						.range([height, 0]);
 
 		svg.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-			.call(d3.axisLeft(yScale));
+			.call(d3.axisLeft(yScale))
+
+		svg.append("text")
+			.attr("class", "titlegraph")
+	        .attr("x", (width / 2))             
+	        .attr("y", (margin.top/2))
+	        .attr("text-anchor", "middle")  
+	        .style("font-size", "16px") 
+	        .text("Percentage of people in (non)compliance with the Dutch fit-norm");
+
+		svg.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 30)
+			.attr("x", 300)
+			.attr("dy", "1em")
+			.style("text-anchor", "start")
+			.text("Value"); 
 
 		// Add lines
 		var line = d3.line()
@@ -50,7 +71,33 @@ function connectedScatter(filepath){
 				.attr("d", function(d){ return line(d.values); })
 				.attr("stroke", function(d){ return colorScale(d.frequency); })
 				.style("stroke-width", 4)
-				.style("fill", "none");
+				.style("fill", "none")
+				.style("opacity", 0);
+
+		//Initiate tooltip
+		var tooltip = d3.select("#tipContainer").append("div")
+		  .attr("class", "tooltip")
+		  .style("opacity", 0);
+
+		// tooltip mouseover event handler
+		var tipShow = function(d) {
+		  var html  = d.year + "<br/>" + d.value + "%";
+
+		  tooltip.html(html)
+		      .style("left", (d3.event.pageX + 15) + "px")
+		      .style("top", (d3.event.pageY - 28) + "px")
+		    .transition()
+		      .duration(200) // ms
+		      .style("opacity", .9) // started as 0!
+
+		};
+		// tooltip mouseout event handler
+		var tipHide = function(d) {
+		  tooltip.transition()
+		      .duration(300) // ms
+		      .style("opacity", 0); // don't care about position!
+		};
+
 
 		// Add dots
 		svg.selectAll("dots")
@@ -66,8 +113,12 @@ function connectedScatter(filepath){
 				.attr("cx", function(d){ return xScale(d.year); })
 				.attr("cy", function(d){ return yScale(d.value)})
 				.attr("r", 5)
-				.attr("stroke", "white");
+				.attr("stroke", "white")
+				.style("opacity", 0)
+				.on("mouseover", tipShow)
+                .on("mouseout", tipHide);
 
+		// Add line labels 
 		svg.selectAll("labels")
 		   .data(data)
 		   .enter()
@@ -79,31 +130,23 @@ function connectedScatter(filepath){
 		   		.attr("x", 12)
 		   		.text(function(d){ return d.frequency; })
 		   		.style("fill", function(d){ return colorScale(d.frequency); })
-		   		.style("font-size", 15);
-
-		svg.selectAll("legend")
-			.data(data)
-			.enter()
-			.append("g")
-			.append("text")
-				.attr("x", function(d,i){ return 50 + (i * 80); })
-				.attr("y", 30)
-				.text(function(d){ return d.frequency; })
-				.style("fill", function(d){ return colorScale(d.frequency); })
-				.style("font-size", 15)
-			.on("click", function(d){
-				currentOpacity = d3.selectAll("." + d.frequency).style("opacity")
-				d3.selectAll("." + d.frequency).transition().style("opacity", currentOpacity == 1 ? 0 : 1)
-			});
-
-
-
-
-
+		   		.style("font-size", 15)
+		   		.style("opacity", 0);
 
 
 	});
+};
 
 
-}
+function changeOpacity(workoutfreq){
+	/*
+	Function to show and hide lines
+	*/ 
+	//
+	currentOpacity = d3.selectAll("." + workoutfreq).style("opacity")
+	d3.selectAll("." + workoutfreq).transition().style("opacity", currentOpacity == 1 ? 0 : 1)
+
+	currentOpacityDots = d3.selectAll("." + workoutfreq).selectAll("circle").style("opacity")
+	d3.selectAll("." + workoutfreq).selectAll("circle").transition().style("opacity", currentOpacity == 1 ? 0 : 1)
+};
 
